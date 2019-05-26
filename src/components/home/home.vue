4572c3abeb5f2afe7d52113bc67a9b7fd85cd8f2
lizhenghua-iri <lizhenghua-iri@360.cn>
2019-05-26 13:58:54 +0800

    update

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,12 +1,16 @@
 <template>
-  <div id="home">shouye</div>
+  <div id="home">
+    <div class="header" v-if="showHeader">header</div>
+    <div class="body">body</div>
+    <div class="footer">footer</div>
+  </div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-
+      showHeader: true
     }
   },
 
@@ -17,5 +21,9 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-
+  #home
+    // .header
+    .body
+      background: url('http://img.sccnn.com/bimg/338/51218.jpg') center center no-repeat
+    // .footer
 </style>
